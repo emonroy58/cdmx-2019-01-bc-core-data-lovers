6d1c5bdbae46c3c7bf2f1e8f2f3390bd9aca0c0e
emonroy58 <emgmonroy@gmail.com>
2019-02-25 04:18:59 -0600

    Funcion que muestra pokemones en pantalla, mediante JSON,cargar menu por Tipo Pokemon, barra responsiva

--- src/data.js
+++ src/data.js
@@ -1,124 +1,18 @@
-// window.worldBank = {
 
-//   filtroPais: (baseDatos, selectPais, indicador) =>{ 
-      
-//       let enviaIndicador=[];
-//       let indicadorFiltro;
-      
-//       const pais =  baseDatos[selectPais].indicators;
-//       //console.log(pais);
-//       const nomIndicador = pais.filter(element=>{ //Funcion filtro selecciona el objeto del pais "MEX" que tiene el elmento indicatorName : Poblacion Total de mujeres
-//         return element.indicatorName === indicador;
-//       })
-//       // console.log(nomIndicador); 
-//       indicadorFiltro=nomIndicador;
-      
-//       // console.log(indicadorFiltro); 
-//       indicadorFiltro.forEach(element => { //forEach recorre arreglo de Indicadores,obteniendo solo la data del elemnto que le pasa el filtro anterior 
-//        let datosIndicador = element.data;
-//       //console.log(datosIndicador); 
-//       enviaIndicador= datosIndicador; 
-//       })  
-//       return enviaIndicador;
-//     },
-  
-//    orderByAsc: (order,arrayOrder) =>{//funcion orden ascendente y descendente
-//    let orderByObject=[];
-  
-//     for (let i in arrayOrder)
-//     {
-//       orderByObject.push([i, arrayOrder[i]]);
-//     }
-//     if (order === "asc")
-//     {
-//       orderByObject.sort(function(a, b) {
-//         //  console.log(a[1] - b[1])
-//         //console.log(orderByObject)
-//         return a[1] - b[1];
-        
-//       });
-//     }else if (order === "desc")
-//      {
-//        orderByObject.sort(function(a, b) {
-//           //console.log(orderByObject)
-//        return b[1] - a[1];      
-//        });
-//     }
-//     return orderByObject;
-//   },
-
-//   VerGrafica: (arrayYear,arrayValors,ctx)=>{//funcion grafica
-
-//     new window.Chart(ctx, { 
-//         type: 'line',
-//         data: {
-//             labels: arrayYear,
-//             datasets: [{
-//                 label: 'Indicador Demografico (%)',
-//                 data: arrayValors,
-//                 backgroundColor: [
-//                     'rgba(77, 169, 197, 0.2)',
-//                     'rgba(54, 162, 235, 0.2)',
-//                     'rgba(255, 206, 86, 0.2)',
-//                     'rgba(75, 192, 192, 0.2)',
-//                     'rgba(153, 102, 255, 0.2)',
-//                     'rgba(255, 159, 64, 0.2)'
-//                 ],
-//                 borderColor: [
-//                     'rgba(11,68,85,1)',
-//                     'rgba(54, 162, 235, 1)',
-//                     'rgba(255, 206, 86, 1)',
-//                     'rgba(75, 192, 192, 1)',
-//                     'rgba(153, 102, 255, 1)',
-//                     'rgba(255, 159, 64, 1)'
-//                 ],
-//                 borderWidth: 1
-//             }]
-//         },
-//         options: {
-//             scales: {
-//                 yAxes: [{
-//                     ticks: {
-//                         beginAtZero:true
-//                     }
-//                 }]
+// window.prueba = {
+//     name: (results) => {
+//         const newArr = results.map(element => {
+//             return obj = {
+//                 name: element.name.toUpperCase(),
+//                 url: element.url
 //             }
-//         }
-//     })
-    
+//         });
+//         return newArr
+//     },
+//     order: (newArr)=> {
+//         let newOrder = newArr.filter( element => element.name ==='STURDY')
+//         return newOrder
 //     }
-  
-//   } //fin de window
-
-const enviar = document.getElementById('enviar')
-const prueba = document.getElementById('prueba')
-
-
-
-
-const url = './data/worldbank/worldbank.json'
-
-fetch(url)
-.then(resp => resp.json())
-.then(result => console.log(result))
-
-// .then(resp =>  resp.json())
-// .then(data => data.results)
-// .then(results => window.prueba.name(results))
-// .then(newArr => window.prueba.order(newArr))
-// .then(newOrder => newOrder)
-// .then(uno =>  enviar.addEventListener('click', () => {
-//   printAll(uno)
-// }))
-
-
-.catch(err => 'error' + err)
-
-// const printAll = (uno) => {
-//   let result = `<p>name : ${uno[0].name}</p>
-//   <a href="${uno[0].url}" target='_blank'> ${uno[0].url}</a>`
-//   prueba.innerHTML = result
 // }
-
 
 
--- src/main.js
+++ src/main.js
@@ -1,147 +1,69 @@
-// const baseDatos =  window.WORLDBANK;
-// const buttonMenu= document.getElementById('showMenu');
-// const menuList= document.getElementById('menuList');
-// const box2= document.getElementById('box2');
-// const box3= document.getElementById('box3');
-// const buttonHome = document.getElementById('home');
-// const buttonIndicador = document.getElementById('indicador');
-// const selOption = document.getElementById('filtrar-pais');
-// const orderAscDat = document.getElementById('order');
-// const nameIndicators= document.getElementById('filtrar-indicador');
-// let contenido = document.getElementById('contenido');
-// let buttonLimpiar = document.getElementById('limpiar');
-// let ctx = document.getElementById("myChart").getContext("2d");//contenido grafica
-// let valorAnioX=[];
-// let valorPorcentajeY=[];
-// let pais;
-// let contador=0;
-
-// const mostrarMenu = () =>{//Funcion mantiene activo o desactivo el menu, segun el valor del contador
-// if(contador==0)
-// {
-//   menuList.style.display= 'block';
-//   contador ++;
-//   }
-//   else
-//   {
-//    menuList.style.display= 'none';
-//   contador=0;
-//  }
-// }
-
-// const menu = () =>{//llama funcion mostrar menu, cuando se da click en Menu
-//     contenido.style.display= 'none';   
-//     mostrarMenu()   
-// }
-// buttonMenu.addEventListener('click', menu);
-
-// const bienvenida = () =>{ //funcion muestra apartado Bienvenida
-//     mostrarMenu()
-//     contenido.style.display= 'none';  
-//     box2.classList.remove('hide');
-//     box3.classList.add('hide');
-// }
-// buttonHome.addEventListener('click', bienvenida);
+let basePokemon={}; //inicializar BD
+const arrayTipoPokemon=[];
+const url = './data/pokemon/pokemon.json'; //ruta del archivo .json
 
 
-// const muestra_indicador = () =>{// funcion muestra apartado indicador
-//     mostrarMenu()
-//     contenido.style.display= 'block';
-//     box2.classList.add('hide');
-//     box3.classList.remove('hide');
-// }
-// buttonIndicador.addEventListener('click', muestra_indicador);
+const btnPokemon = document.getElementById('btn-Pokemon');
+const productWrapper = document.getElementById('list-pokemon');
+const viewPokemons = () =>{  //muestra pokemon por tipo
+const basePokemon = JSON.parse(localStorage.getItem('data'));
+const base = basePokemon.pokemon;
+      let viewAllPokem = '';
+      for(let i in base) {           
+        //console.log('Tipo Poekemon: ',pokemon);
+        viewAllPokem = viewAllPokem +`  
+        <div class="col-md-3">
+         <div class="card bg-dark">
+           <img src="${base[i].img}" class="card-img-top" alt="${base[i].name}">
+           <div class="card-body">
+           <h5 id="product-name" class="card-title d-flex justify-content-center">${base[i].name}</h5>
+           </div>
+         </div> 
+        </div>             
+        `
+      arrayTipoPokemon.push(base[i].type[0]); //almacena tipo de pokemon en array
+      }
+      //arrayDataPokemTipo almacena elementos unicos menu Filtrar, por mecio de filter
+      let arrayDataPokemTipo = arrayTipoPokemon.filter(function(item, index, array) {
+        return array.indexOf(item) === index;
+      })
+      //console.log(arrayDataPokemTipo);
+      dataFilter(arrayDataPokemTipo)
+
+    productWrapper.innerHTML=viewAllPokem;    
+    return viewAllPokem;  
+}
+btnPokemon.addEventListener('click', viewPokemons);
 
 
-// const filtraIndPais = () =>{
-//     valorAnioX=[]; //inicializa los arreglo eje X para el pintado de la grafica
-//     valorPorcentajeY=[]; //inicializa los arreglo eje Y para el pintado de la grafica
-//     contenido.style.display= 'block';
-//     let selectPais= selOption.value; //Select optiene el valor de cada value "MEX", "PER","BRA","CHL"
-//     let indicador = nameIndicators.value;
-//     //console.log(select);
-//      pais = window.worldBank.filtroPais(baseDatos,selectPais,indicador); //Se llama la funcion window.filtroPais con el valor de selec
-//      recorrerObjeto (pais); 
-// }
-// nameIndicators.addEventListener('change',filtraIndPais); //funcion recorre el objeto
+//funcion llenado de menu filtro
+menuFiltro = document.getElementById('menu-filtro');
+const dataFilter =(arrayDataPokemTipo) =>{
+  let llenado = '';
+  for(let i=0; i<arrayDataPokemTipo.length; i++){
+  llenado = llenado +`
+  <a class="dropdown-item" href="#">${arrayDataPokemTipo[i]}</a>
+  `
+  }  
+  menuFiltro.innerHTML = llenado;
+}//fin funct llenado
 
 
-// const recorrerObjeto = (pais)=>{  
-
-// let respuesta="<li>"+"<font size=4.5>"+"<b>"+"Año "+"&nbsp"+"&nbsp"+"  Valor(%)"+"</font>"+"</li>"+"</b>"+"<br>";
-
-//  for (let i in pais) //for iteracon cada elemento del objeto que se almacena en la variable pais
-//   {
-//     respuesta+="<li>"+"<b>"+i+": "+"&nbsp;&nbsp;&nbsp;"+" </b>"+pais[i].toFixed(2)+"%"+"</li>"+"<br>"; //cada elemnto del objeto se muestra en li, usando toFixed para delimitar decimales
-//    }
-//     for (let i in pais) 
-//     {//recorre objeto para pasar el año a nuevo arrego de eje X
-//     valorAnioX.push(i);
-//     }  
-//     for (let j in pais)
-//    {//recorre objeto para pasar el año a nuevo arrego de eje Y
-//    valorPorcentajeY.push(pais[j]);
-//   }
-//  document.getElementById('contenido').innerHTML = respuesta;
-//  document.getElementById('graf').style.display='block';//comentar grafica
-//  DatGraph();
-//  return respuesta;
-// }
-
-// const prtOrder=(orderObj)=>{
-// let respOrder="<li>"+"<b>"+"Año "+"&nbsp"+"&nbsp"+"  Valor(%)"+"</li>"+"</b>"+"<br>";    
-// for (let i in orderObj)
-//  {
-//   respOrder+="<li>"+"<b>"+orderObj[i][0]+":  "+"</b>"+"&nbsp"+"&nbsp"+parseFloat(orderObj[i][1]).toFixed(2)+"%"+"</li>"+"<br>";
-//  }     
-//  document.getElementById('contenido').innerHTML = respOrder;
-//  return  respOrder;
-// }
-
-// const orderByAscent = () =>{
-//     let arrayOrder=pais;
-//     let order= orderAscDat.value;
-//     //console.log(order);
-//     let orderObj = window.worldBank.orderByAsc(order,arrayOrder)
-//     //console.log(orderObj);
-//     prtOrder(orderObj)
-// }
-// orderAscDat.addEventListener('change',orderByAscent); //funcion recorre el objeto*/
-
-// const limpiar = () => {
-//     document.getElementById('filtrar-pais').value = '';
-//     document.getElementById('filtrar-indicador').value = '';
-//     document.getElementById('order').value = '';
-//     document.getElementById('contenido').innerHTML = '';
-//     document.getElementById('graf').style.display='none';
-//   } 
-
-//   buttonLimpiar.addEventListener('click', limpiar)
-// //console.log(limpiar);
 
 
-// const DatGraph = () =>{//funcion llenado de grafica con arrayX y arrayY
-// let arrayYear = valorAnioX;
-// let arrayValors = valorPorcentajeY;
-// let prtGrap =ctx;
-
-// let almacenaGraf= window.worldBank.VerGrafica(arrayYear,arrayValors,prtGrap)
-// return almacenaGraf;
-// }
 
 
-// window.prueba = {
-//     name: (results) => {
-//         const newArr = results.map(element => {
-//             return obj = {
-//                 name: element.name.toUpperCase(),
-//                 url: element.url
-//             }
-//         });
-//         return newArr
-//     },
-//     order: (newArr)=> {
-//         let newOrder = newArr.filter( element => element.name ==='STURDY')
-//         return newOrder
-//     }
-// }
+fetch(url)   //lectura del archivo .json
+.then(response => response.json())
+.then(data => { localStorage.setItem('data',JSON.stringify(data)) }) //procesa datos
+ //console.log (data);    
+.catch(err => (err)) 
+
+/*function getPokemon () {
+  const stringPokemon = localStorage.getItem('data');
+  const basePokemon = JSON.parse(stringPokemon);
+  viewPokemons(basePokemon)
+  //return basePokemon;
+}*/
+    
+  
